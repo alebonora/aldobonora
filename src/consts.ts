8056af84540c286606fbd4d41ab3e2b71c9c0cc4
alebonoraPS <alebonora78@gmail.com>
2025-03-15 15:57:29 +0200

    Adding files for build

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Aldo Bonora';
+export const SITE_DESCRIPTION = 'Welcome to the website of the Italian neo-realist painter, Aldo Bonora!';
+export const GRAPHQL_URL = 'http://astro-test.local/graphql';
+export const SITE_URL = 'http://localhost:4321/';
